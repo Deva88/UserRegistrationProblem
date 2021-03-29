dba55f51957bd628ef1294a1b78f39ec6ad18fcb
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-29 21:25:27 +0530

    Added: First Name Valid or Not.

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -1,17 +1,19 @@
 package com.userregistration;
+
 import java.util.*;
 import java.util.regex.*;
+
 public class UserRegistration {
 	/**
-	 * * UC1-Validate first name
-	 * user need to enter a valid first name Start with Cap and has minimum 3 characters
+	 * * UC1-Validate first name user need to enter a valid first name Start with
+	 * Cap and has minimum 3 characters
 	 */
 	public static boolean firstName() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter First Name: ");
 		String first = sc.next();
-		//Using REGEX
-		if(Pattern.matches("^[A-Z][aA-zZ]{2,}", first)) {
+		// Using REGEX
+		if (Pattern.matches("^[A-Z][aA-zZ]{2,}", first)) {
 			System.out.println("First Name valid");
 			return true;
 		} else {
@@ -19,6 +21,7 @@ public class UserRegistration {
 			return firstName();
 		}
 	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration");
 		firstName();
