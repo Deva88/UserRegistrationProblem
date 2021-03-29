b64f955a3d7bdd3104f15da3d7aac71a48a95741
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-30 00:20:44 +0530

    Added: Mobile Format with country code

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -2,16 +2,21 @@ package com.userregistration;
 
 import java.util.*;
 import java.util.regex.*;
-
+/*
+@description: class to checking user Phone Number 
+by regular expression
+@parameters: defining a method mobileNo() to
+check Phone Number is valid with country code using regular expression 
+ */
 public class UserRegistration {
-	/**
-	 * * UC3-Validate Email user need to enter a valid Email ID valid or not.
-	 * 
-	 */
+
+	
+	//method to check Phone Number is valid or not by using regular expression
 	public static boolean mobileNo() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter mobile number: ");
 		String mobile = sc.next();
+		// Using REGEX
 		if (Pattern.matches("(0|91)?[6-9][0-9]{9}", mobile)) {
 			System.out.println("valid");
 			return true;
