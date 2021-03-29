268f99f2af8f0e1347464cdc571e44dbb485e77b
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-30 02:35:39 +0530

    Added: email sample

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -4,37 +4,32 @@ import java.util.*;
 import java.util.regex.*;
 
 /*
-@description: class to checking Password  
+@description: class to checking Email Samples  
 by regular expression
 
-@parameters: defining a method passwordCheck() to check Password 
-Minimum 8 Character, 
-least 1 Upper case and 
-least 1 numeric number 
-1 Special Character using regular expression 
+@parameters: defining a method email() to check email using regular expression
+ Validate Email user need to enter a valid Email ID valid or not. 
  */
 public class UserRegistration {
 
-	// method to check Password is valid or not by using regular expression
-	public static boolean passwordCheck() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter minimum 8 Character password : ");
-		String password = sc.next();
+	// method to check Email is valid or not by using regular expression
+		public static boolean email() {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter email: ");
+			String email = sc.next();
+			if (Pattern.matches("[a-z A-Z 0-9][a-z A-Z 0-9_.]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+", email)) {
 
-		// Regular Expressions
-		if (Pattern.matches("[\\S]{8,}", password) && Pattern.matches(".*[A-Z].*", password)
-				&& Pattern.matches(".*[0-9].*", password) && Pattern.matches(".*[\\W]{1}.*", password)) {
-			System.out.println("Password valid");
-			return true;
-		} else {
-			System.out.println("Password invalid");
-			return passwordCheck();
+				System.out.println("Email Id valid");
+				return true;
+			} else {
+				System.out.println("Email Id invalid");
+				return email();
+			}
 		}
-	}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to user registration");
-		passwordCheck();
-	}
+		public static void main(String[] args) {
+			System.out.println("Welcome to user registration");
+			email();
+		}
 
-}
+	}
