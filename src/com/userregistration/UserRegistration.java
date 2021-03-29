1c47b10e364ac82268ad3cbd2679b24eb58f462c
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-30 01:05:28 +0530

    Added: Password Minimum 8 Characters

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -2,33 +2,34 @@ package com.userregistration;
 
 import java.util.*;
 import java.util.regex.*;
+
 /*
-@description: class to checking user Phone Number 
+@description: class to checking Password  
 by regular expression
-@parameters: defining a method mobileNo() to
-check Phone Number is valid with country code using regular expression 
+@parameters: defining a method passwordCheck() to
+check Password minimum 8 Characters using regular expression 
  */
 public class UserRegistration {
 
-	
-	//method to check Phone Number is valid or not by using regular expression
-	public static boolean mobileNo() {
+	// method to check Password is valid or not by using regular expression
+	public static boolean passwordCheck() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter mobile number: ");
-		String mobile = sc.next();
-		// Using REGEX
-		if (Pattern.matches("(0|91)?[6-9][0-9]{9}", mobile)) {
-			System.out.println("valid");
+		System.out.println("Enter minimum 8 Character password : ");
+		String password = sc.next();
+
+		// Regular Expressions
+		if (Pattern.matches("[a-zA-Z0-9]{8,}", password)) {
+			System.out.println("Password valid");
 			return true;
 		} else {
-			System.out.println("Invalid");
-			return mobileNo();
+			System.out.println("Password invalid");
+			return passwordCheck();
 		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration");
-		mobileNo();
+		passwordCheck();
 	}
 
 }
